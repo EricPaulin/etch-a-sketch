d57ee8d19376196feeceb37b295be74f437adec1
Eric <ericpaulin78055@gmail.com>
2022-04-30 17:28:29 -0700

    Added index, script, and style

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+const content2 = document.createElement('div');
+const content3 = document.createElement('div');
+const content4 = document.createElement('div');
+content.classList.add('content');
+
+container.appendChild(content);
