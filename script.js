60d3f9e9cd1ff3a294bfdfe0a12176464280191f
Eric <ericpaulin78055@gmail.com>
2022-04-30 20:16:02 -0700

    adding grids

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ const content2 = document.createElement('div');
 const content3 = document.createElement('div');
 const content4 = document.createElement('div');
 content.classList.add('content');
+content.classList.add('content2');
+content.classList.add('content3');
+content.classList.add('content4');
 
 container.appendChild(content);
+container.appendChild(content4);
+container.appendChild(content3);
+container.appendChild(content2);
