2375e06beb4f674a906136984174c0b2cadeb10f
Eric <ericpaulin78055@gmail.com>
2022-05-01 17:13:49 -0700

    Updated Logo, Added Background, Buttons

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # etch-a-sketch
 
-Animated Logo made using Textanim -> https://textanim.com/
+Special Thanks <3
+Night Sky Gradient Background
+    Made By: flickerfae (aka pintsizedwitch)
+    https://flickerfae.tumblr.com/post/93460308187/subtly-animated-pixel-night-sky-background-i-did
+
+Etch-a-Sketch Font
+    Made Using: Textanium
+    https://textanim.com/
--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const container = document.getElementById("container");
+const square = document.getElementById("square");
 
 // Creates Desired Row x Column size
 function makeGrid(rows, cols) 
 {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
+    square.style.setProperty('--grid-rows', rows);
+    square.style.setProperty('--grid-cols', cols);
     for (let i = 0; i < (rows * cols); i++)
     {
         let cell = document.createElement("div");
-        container.appendChild(cell).className = "cell";
+        square.appendChild(cell).className = "cell";
     }
 }
 
-makeGrid(10, 10);
+makeGrid(16, 16);
 
-container.addEventListener('mouseover', function (e) 
+square.addEventListener('mouseover', function (e) 
 {
     e.target.style.background = 'black';
 });
